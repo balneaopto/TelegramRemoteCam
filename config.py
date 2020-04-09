3c20279af2d6a0275267968b75980c11fcfb7ec7
balneaopto <51031809+balneaopto@users.noreply.github.com>
2020-04-09 22:40:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 MSG_START = "Remote CAM ACTIVATED!"
 MSG_STOP = "Remote CAM TERMINATED!"
 MSG_FAILED = "WARNING! NO image sent  "
-#Set time interval in seconds between two snapshots
+#Set time interval in seconds between two snapshots. Change value at your convenience.
 INTERVAL = 900
