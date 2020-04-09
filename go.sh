d99ef5e5f32d252d02b5abc46d98aa5a11d041d1
balneaopto <51031809+balneaopto@users.noreply.github.com>
2020-04-09 22:42:12 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #sleep 60
 remotecam/remotecam.sh start
-#Set duration of remote control in seconds
+#Set duration of remote control in seconds. Change value at your convenience.
 sleep 4000
 remotecam/remotecam.sh stop
 remotecam/endmessage.py
-
-
